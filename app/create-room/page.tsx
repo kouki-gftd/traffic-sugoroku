a7269fb4e865fd4ca2ee0bcb550dd2142d9bdf78
kouki-gftd <k.kanamori@gftd.works>
2024-03-29 15:18:16 +0900

    ルームの新規作成ページのUIの実装

--- app/create-room/page.tsx
+++ app/create-room/page.tsx
@@ -1,6 +1,36 @@
 const Page = () => {
   return (
-    <div>create page</div>
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h1 className="mt-8 ml-10 text-4xl text-white font-bold">SUGOROKU</h1>
+        <h1 className="mt-8 mr-10 text-4xl text-white font-bold">View Result</h1>
+      </div>
+      <div className="create-new-room-container">
+        <div className="my-10 text-5xl font-bold">
+          Create New Room
+        </div>
+        <p className="mt-5 mb-5 text-2xl font-bold">Choose Game Mode</p>
+        <div className="game-mode">
+          <div>
+            <h1>Basic</h1>
+            <p>Who arrives the fastest?</p>
+          </div>
+          <div>
+            <h1>Aging Society</h1>
+            <p>Elderly people can only use trains</p>
+          </div>
+          <div>
+            <h1>Mountain Climbing</h1>
+            <p>Goal early with everyone</p>
+          </div>
+          <div>
+            <h1>Viewing Cherry Blossoms</h1>
+            <p>Two of you must quickly reserve a nice spot</p>
+          </div>
+        </div>
+        <button className="my-5 px-20 py-3 bg-buttonColor font-bold rounded-lg text-white">Next</button>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        "buttonColor": "#D9D9D9"
+        "buttonColor": "#ABA8A8"
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
