694cd5055f5f50661ef7a441b2ac297a1a4b3fce
kouki-gftd <k.kanamori@gftd.works>
2024-05-21 14:02:08 +0900

    page.tsxのリファクタリングと車カードの進行をブラウザに反映

--- components/CalculateCarMoveSpaces.tsx
+++ components/CalculateCarMoveSpaces.tsx
@@ -1,52 +1,16 @@
 import React from "react";
+import { calculateMoveSpaces } from "./Gamerules";
 
-const gameRules = [
-  {players: 4, carCount: 1, moveSpaces: 3},
-  {players: 4, carCount: 2, moveSpaces: 2},
-  {players: 4, carCount: 3, moveSpaces: 1},
-  {players: 4, carCount: 4, moveSpaces: 0},
-  {players: 5, carCount: 1, moveSpaces: 3},
-  {players: 5, carCount: 2, moveSpaces: 2},
-  {players: 5, carCount: 3, moveSpaces: 1},
-  {players: 5, carCount: 4, moveSpaces: 1},
-  {players: 5, carCount: 5, moveSpaces: 0},
-  {players: 6, carCount: 1, moveSpaces: 3},
-  {players: 6, carCount: 2, moveSpaces: 3},
-  {players: 6, carCount: 3, moveSpaces: 2},
-  {players: 6, carCount: 4, moveSpaces: 1},
-  {players: 6, carCount: 5, moveSpaces: 0},
-  {players: 6, carCount: 6, moveSpaces: 0},
-  {players: 7, carCount: 1, moveSpaces: 4},
-  {players: 7, carCount: 2, moveSpaces: 3},
-  {players: 7, carCount: 3, moveSpaces: 2},
-  {players: 7, carCount: 4, moveSpaces: 1},
-  {players: 7, carCount: 5, moveSpaces: 1},
-  {players: 7, carCount: 6, moveSpaces: 0},
-  {players: 7, carCount: 7, moveSpaces: 0},
-  {players: 8, carCount: 1, moveSpaces: 4},
-  {players: 8, carCount: 2, moveSpaces: 3},
-  {players: 8, carCount: 3, moveSpaces: 2},
-  {players: 8, carCount: 4, moveSpaces: 2},
-  {players: 8, carCount: 5, moveSpaces: 1},
-  {players: 8, carCount: 6, moveSpaces: 1},
-  {players: 8, carCount: 7, moveSpaces: 0},
-  {players: 8, carCount: 8, moveSpaces: 0},
-];
-
-type CalculationLogicProps = {
+type CalculateCarMoveSpacesProps = {
   playerCount:  number;
   carCardCount: number;
 };
 
-const CalculateCarMoveSpaces: React.FC<CalculationLogicProps> = ({ playerCount, carCardCount }) => {
+const CalculateCarMoveSpaces: React.FC<CalculateCarMoveSpacesProps> = ({ playerCount, carCardCount }) => {
   // プレイヤー数と車カードの枚数に基づいて進むマス数を計算する関数
-  const calculateMoveSpaces = () => {
-    const rule = gameRules.find(rule => rule.players === playerCount && rule.carCount === carCardCount);
-    return rule ? rule.moveSpaces : 0;
-  };
   return (
     <div>
-      +{calculateMoveSpaces()}
+      +{calculateMoveSpaces(playerCount, carCardCount)}
     </div>
   );
 };
