fa0862e23f93fbaa0bf597861e33e06ef012dbbc
kouki-gftd <k.kanamori@gftd.works>
2024-05-30 15:21:35 +0900

    /result画面のレイアウト

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -32,7 +32,7 @@ const Page: React.FC = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        <div className="w-3/4 md:my-10">
+        <div className="w-4/5 md:my-10">
           <div className="md:my-5 flex justify-around text-blue-900 text-sm md:text-2xl font-bold">
             <div>
               Total Cards
@@ -44,24 +44,32 @@ const Page: React.FC = () => {
           <div className="text-blue-900 font-bold text-sm md:text-xl">
             Mode Basic
           </div>
-          <div className="ml-10 flex font-bold text-xl md:text-4xl text-white">
-            <div className="flex flex-col">
-              {playerData.map((player, playerIndex) => (
-                <li key={playerIndex} className={`flex flex-col max-w-36 p-1 mb-3 font-bold text-2xl text-center text-white rounded-lg ${player.color}`}>
-                  {player.name}
-                </li>
-              ))}
-            </div>
+          <div className="font-bold flex flex-row justify-end items-center text-xl md:text-4xl text-white">
             {stationNames.map((station, stationIndex) => (
-              <div key={stationIndex} className="m-2 md:m-5">
-                <div className="text-center">{stationIndex + 1}</div>
-                <div className="flex flex-col items-center">
-                  {playerData.map((player, playerIndex) => (
+              <div key={stationIndex} className="mx-10 ">
+                {stationIndex + 1}
+              </div>
+            ))}
+            <div className="m-5 flex flex-row">
+              <div>Rank</div>
+              <div className="ml-5">CO2</div>
+            </div>
+          </div>
+          <div className="flex flex-col font-bold text-xl md:text-4xl text-white">
+            {playerData.map((player, playerIndex) => (
+              <div key={playerIndex} className="flex items-center">
+                <div className={`flex-shrink-0 flex flex-col items-center w-15 md:w-36 py-3 mb-5 font-bold text-sm md:text-2xl text-center text-white rounded-lg ${player.color}`}>
+                  {player.name}
+                </div>
+                <div className="flex flex-wrap flex-row justify-center ml-8">
+                  {stationNames.map((station, stationIndex) => (
                     player.cardHistory[stationIndex] && (
-                      <div key={`${playerIndex}-${stationIndex}`} className="mb-5">
+                      <div key={stationIndex} className="flex flex-col items-center mx-4">
                         <img
                           src={player.cardHistory[stationIndex]}
                           alt={`card ${stationIndex + 1}`}
+                          width={70}
+                          height={70}
                           className="mb-1"
                         />
                         <div className="text-center text-sm text-white">
@@ -73,16 +81,11 @@ const Page: React.FC = () => {
                 </div>
               </div>
             ))}
-            <div className="m-5 flex flex-row">
-              <div>Rank</div>
-              <div className="ml-5">CO2</div>
-            </div>
           </div>
           <div>
           </div>
         </div>
       </div>
-
     </>
   );
 };
