34a2de1ff923024fb7bd57bd14ff491f41350072
kouki-gftd <k.kanamori@gftd.works>
2024-06-06 13:34:51 +0900

    Co₂排出量の計算機能の実装

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { stationNames } from "../play-sugoroku/page";
 import Link from "next/link";
+import CalculateOfCo2 from "@/components/CalculateOfCo2";
 
 type playerData = {
-  name:         string;
-  color:        string;
-  cardHistory:  string[];
+  name: string;
+  color: string;
+  cardHistory: string[];
   stepsHistory: number[];
 };
 
 const Page: React.FC = () => {
   const [playerData, setPlayerData] = useState<playerData[]>([]);
+  const [totalCo2, setTotalCo2] = useState<number>(0);
 
   useEffect(() => {
     const storedPlayerData = localStorage.getItem('players');
@@ -27,6 +29,11 @@ const Page: React.FC = () => {
       return total + player.cardHistory.length;
     }, 0);
   };
+
+  // CO2排出量を集計するコールバック関数
+  const handleCalculateCo2 = useCallback((co2Emission: number) => {
+    setTotalCo2(prevTotal => prevTotal + co2Emission);
+  }, []);
 
   return (
     <>
@@ -50,53 +57,73 @@ const Page: React.FC = () => {
               Total Cards {totalCards()}
             </div>
             <div>
-              Total CO2 Emission XX
+              Total CO2 Emission XX {totalCo2}
             </div>
           </div>
           <div className="text-blue-900 font-bold text-sm md:text-xl">
             Mode Basic
           </div>
-          <div className="font-bold flex flex-row justify-end items-center text-xl md:text-4xl text-white">
-            {stationNames.map((station, stationIndex) => (
-              <div key={stationIndex} className="mx-10 ">
-                {stationIndex + 1}
-              </div>
-            ))}
-            <div className="m-5 flex flex-row items-center">
-              <div className="flex flex-col items-center">
-                <div>Rank</div>
-              </div>
-              <div className="ml-5">CO2</div>
-            </div>
-          </div>
-          <div className="flex flex-col font-bold text-xl md:text-4xl text-white">
-            {playerData.map((player, playerIndex) => (
-              <div key={playerIndex} className="flex items-center">
-                <div className={`flex-shrink-0 flex flex-col items-center w-15 md:w-36 py-3 mb-5 font-bold text-sm md:text-2xl text-center text-white rounded-lg ${player.color}`}>
+          <div className="font-bold flex flex-row justify-end text-xl md:text-4xl text-white">
+            <div className="mt-20 pt-3">
+              {playerData.map((player, playerIndex) => (
+                <div
+                  key={playerIndex}
+                  className={`w-15 md:w-36 py-3 mb-14 font-bold text-sm md:text-2xl text-center rounded-lg ${player.color}`}
+                >
                   {player.name}
                 </div>
-                <div className="flex flex-wrap flex-row justify-center ml-8">
-                  {stationNames.map((station, stationIndex) => (
-                    player.cardHistory[stationIndex] && (
-                      <div key={stationIndex} className="flex flex-col items-center mx-4">
-                        <img
-                          src={player.cardHistory[stationIndex]}
-                          alt={`card ${stationIndex + 1}`}
-                          width={70}
-                          height={70}
-                          className="mb-1"
-                        />
-                        <div className="text-center text-sm text-white">
-                          +{player.stepsHistory[stationIndex]}
+              ))}
+            </div>
+            {stationNames.map((station, stationIndex) => (
+              <div>
+                <div key={stationIndex} className="mx-10 m-5">
+                  {stationIndex + 1}
+                </div>
+                <div>
+                  <div>
+                    {playerData.map((player, playerIndex) => (
+                      player.cardHistory[stationIndex] && (
+                        <div key={stationIndex} className="flex flex-col items-center mx-4">
+                          <img
+                            src={player.cardHistory[stationIndex]}
+                            alt={`card ${stationIndex + 1}`}
+                            width={70}
+                            height={70}
+                            className="mb-1"
+                          />
+                          <div className="text-center text-sm text-white">
+                            +{player.stepsHistory[stationIndex]}
+                          </div>
                         </div>
-                      </div>
-                    )
+                      )
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+            <div className="m-5 flex flex-row">
+              <div className="flex flex-col items-center">
+                <div>
+                  Rank
+                </div>
+              </div>
+              <div className="ml-5 flex flex-col">
+                CO2
+                <div>
+                  {playerData.map((player, playerIndex) => (
+                    <div key={playerIndex} className="flex flex-col mt-10 pb-8">
+                      <CalculateOfCo2
+                        name={player.name}
+                        color={player.color}
+                        cardHistory={player.cardHistory}
+                        stepsHistory={player.stepsHistory}
+                        onCalculate={handleCalculateCo2}
+                      />
+                    </div>
                   ))}
                 </div>
               </div>
-            ))}
-          </div>
-          <div>
+            </div>
           </div>
         </div>
       </div>
